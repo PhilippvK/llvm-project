7b71c026f734027093c95b0430f21b1ae8935dd7
Max Mustermann <max@mustermann.com>
2024-12-15 14:17:24 +0100

    GPR32V feature, register class and heuristics

--- llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
+++ llvm/lib/Target/RISCV/RISCVTargetTransformInfo.h
@@ -332,7 +332,7 @@ public:
 
   unsigned getRegisterClassForType(bool Vector, Type *Ty = nullptr) const {
     if (Vector)
-      return RISCVRegisterClass::VRRC;
+      return ST->hasGPR32V() ? RISCVRegisterClass::GPRRC : RISCVRegisterClass::VRRC;
     if (!Ty)
       return RISCVRegisterClass::GPRRC;
 
